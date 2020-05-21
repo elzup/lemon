a630f90fa6b793b9cb0e7b8ddd6831ddfa8b1a5c
elzup <guild0105@gmail.com>
2020-05-21 15:28:19 +0900

    make color picker

--- src/components/AppIcon/index.tsx
+++ src/components/AppIcon/index.tsx
@@ -1,15 +1,51 @@
 import { Typography, Container } from '@material-ui/core'
 import styled from 'styled-components'
+import { SketchPicker, SwatchesPicker } from 'react-color'
 import App from '../App'
 import AuthContainer from '../AuthContainer'
 
-const Style = styled.div``
+const Style = styled.div`
+  .img-grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    > div {
+      img {
+        width: 100%;
+      }
+      border: solid gray 4px;
+      border-radius: 12px;
+      &[data-select] {
+        border: solid orange 4px;
+      }
+    }
+  }
+
+  .swatches-picker {
+    > div > div > div {
+      height: 150px !important;
+    }
+    height: 150px !important;
+    width: 100% !important;
+  }
+`
 
 function Main() {
   return (
     <Style>
       <Container>
         <Typography variant="h5">アイコンメイク</Typography>
+        <Typography variant="h6">イメージ</Typography>
+        <div className="img-grid">
+          <div data-select>
+            <img src="/baseicon/lemon-atsu-face.png" />
+          </div>
+        </div>
+        <Typography variant="h6">背景色</Typography>
+        <SwatchesPicker
+          onChange={(e) => {
+            console.log(e)
+          }}
+        />
       </Container>
     </Style>
   )
---
+++ src/utils.ts
@@ -0,0 +1,8 @@
+function range(start: number, end: number, step: number) {
+  const arr: number[] = []
+
+  for (let i = start; i < end; i += step) {
+    arr.push(i)
+  }
+  return arr
+}
